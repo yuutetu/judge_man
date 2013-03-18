eb6ec01e25b8508f7f5e21d116c260adc86005bf
加賀江 優幸 <math16.music25@gmail.com>
2013-03-18 09:27:30 +0900

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JudgeMan::Application.config.secret_key_base = '3b6416b5049c4dc5ddc72fc3edfecc897d1a2881bc3a7e5112af17d9068732ea40c5b828cddcd0d49f8e565ed2eb65faccf836b8fee018c1196313d6381625d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JudgeMan::Application.config.session_store :encrypted_cookie_store, key: '_JudgeMan_session'
