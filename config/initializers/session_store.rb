bf80df095f8714b43ba8bca37e8d666613cac75d
加賀江 優幸 <math16.music25@gmail.com>
2013-05-23 16:03:42 +0900

    update rails4.0.0-rc1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-JudgeMan::Application.config.session_store :encrypted_cookie_store, key: '_JudgeMan_session'
+JudgeMan::Application.config.session_store :cookie_store, key: '_JudgeMan_session'
